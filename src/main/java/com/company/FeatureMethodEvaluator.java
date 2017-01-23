e0b9f9bca018137096d2ed16888d7b903265d600
Marten Sigwart <marten.sigwart@haw-hamburg.de>
2017-01-23 15:46:05 +0100

    added first evaluator (still empty)

---
+++ src/main/java/com/company/FeatureMethodEvaluator.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.List;
+
+/**
+ * Created by martensigwart on 23.01.17.
+ */
+public class FeatureMethodEvaluator implements IEvaluator {
+
+    public void evaluate(List<FSResult> results) {
+
+    }
+}
