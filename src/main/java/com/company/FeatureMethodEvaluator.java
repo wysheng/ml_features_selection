b04f2174f64b185f2b238134619c253793259c8a
Marten Sigwart <marten.sigwart@haw-hamburg.de>
2017-01-23 17:27:38 +0100

    created method getXYDataSet

--- src/main/java/com/company/FeatureMethodEvaluator.java
+++ src/main/java/com/company/FeatureMethodEvaluator.java
@@ -1,6 +1,13 @@
 package com.company;
 
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by martensigwart on 23.01.17.
@@ -8,6 +15,58 @@ import java.util.List;
 public class FeatureMethodEvaluator implements IEvaluator {
 
     public void evaluate(List<FSResult> results) {
+
+        XYDataset dataset = getXYDataSet(results);
+
+    }
+
+    private XYDataset getXYDataSet(List<FSResult> results) {
+        Map<String, XYSeries> seriesMap = new HashMap<String, XYSeries>();
+
+        //sort results by method
+        Map<String, List<FSResult>> resultsByMethod = results
+            .stream()
+            .collect(Collectors.groupingBy(s -> s.fsMethod));
+
+        resultsByMethod
+            .forEach((fsmethod, fsResults) -> {
+
+                //sort results by number of features
+                Map<Integer, List<FSResult>> noFeatureResults = fsResults
+                    .stream()
+                    .collect(Collectors.groupingBy(o -> o.numOfFeatures));
+
+                //Average results for each number of features
+                noFeatureResults
+                    .forEach((i, results1) -> {
+
+                        Double avrg = results1.stream()
+                            .collect(Collectors.averagingDouble(result -> result.evaluation.correct()));
+
+
+                        //check if series already exists, if not create it
+                        XYSeries series;
+                        if (seriesMap.containsKey(fsmethod)) {
+                            series = seriesMap.get(fsmethod);
+                        } else {
+                            series = new XYSeries(fsmethod);
+                            seriesMap.put(fsmethod, series);
+                        }
+
+                        //add or update rightly classified percentage
+                        series.addOrUpdate(i, avrg);
+
+                    });
+
+            });
+
+        //add series to data set
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        seriesMap.forEach((name, xyseries) -> {
+            dataset.addSeries(xyseries);
+        });
+
+        return dataset;
 
     }
 }
